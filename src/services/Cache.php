63b48c7a297cadf01f2214b546183bc956d396e9
Andrew Welch <andrew@nystudio107.com>
2022-03-13 00:21:38 -0500

    feat: Added the ability to use environment variables and aliases in the **FastCGI Cache Path** setting ([#9](https://github.com/nystudio107/craft-fastcgicachebust/issues/9))

--- src/services/Cache.php
+++ src/services/Cache.php
@@ -10,12 +10,11 @@
 
 namespace nystudio107\fastcgicachebust\services;
 
-use nystudio107\fastcgicachebust\FastcgiCacheBust;
-use nystudio107\fastcgicachebust\models\Settings;
-
 use Craft;
 use craft\base\Component;
 use craft\helpers\FileHelper;
+use nystudio107\fastcgicachebust\FastcgiCacheBust;
+use nystudio107\fastcgicachebust\models\Settings;
 use yii\base\ErrorException;
 
 /**
@@ -40,6 +39,7 @@ class Cache extends Component
         if (!empty($settings) && !empty($settings->cachePath)) {
             $cacheDirs = explode(',', $settings->cachePath);
             foreach ($cacheDirs as $cacheDir) {
+                $cacheDir = Craft::parseEnv($cacheDir);
                 $cacheDir = trim($cacheDir);
                 try {
                     FileHelper::clearDirectory($cacheDir);
@@ -49,7 +49,7 @@ class Cache extends Component
                 Craft::info(
                     Craft::t(
                         'fastcgi-cache-bust',
-                        'FastCGI Cache busted: `'.$cacheDir
+                        'FastCGI Cache busted: `' . $cacheDir
                     ),
                     __METHOD__
                 );
